9aa11848946a1834e22d2fb5acb0ddd4e0f6fb15
MatiasBarrios <soymatiasbarrios@gmail.com>
2019-03-24 01:55:55 -0300

    master

--- find_and_send.sh
+++ find_and_send.sh
@@ -3,7 +3,8 @@
 FOLDER='test_folder'
 EXTENSION='*.txt'
 
-rm -f mail.html
+rm -f mail.html found.txt
+echo 0 > found.txt
 
 echo "
 From:$(whoami)@$(hostname)
@@ -13,9 +14,44 @@ MIME-Version: 1.0
 Content-Type: text/html
 
 <html>
+<style>
+thead,
+tfoot {
+    background-color: rgb(52, 96, 218);
+    color: #fff;
+}
+
+tbody {
+    background-color: #e4f0f5;
+    border: 2px solid rgb(200, 200, 200);
+}
+
+th, td {
+  border: 1px solid #bbb;
+  padding: 2px 8px 0;
+  text-align: left;
+}
+
+caption {
+    padding: 10px;
+    caption-side: bottom;
+}
+
+table {
+    border-collapse: collapse;
+    border: 2px solid rgb(200, 200, 200);
+    letter-spacing: 1px;
+    font-family: sans-serif;
+    font-size: .8rem;
+    width: 80%;
+}
+</style>
 <body>
-<table style=\"border-style:solid;border-color:BLACK;border-size: 1px 2px 3px 5px;\">
-<thead style=\"background-color:#2f97c4;border-style:solid; font-color:WHITE;\" >
+
+<p><b>Some keywords matched at $(date)</b></p>
+
+<table>
+<thead>
 <tr>
 <th>
 Keyword
@@ -26,11 +62,11 @@ Subject
 <th>
 Date
 </th>
-</tr>" >> mail.html
+</tr>
+</thead>
+<tbody>" >> mail.html
 
 all_files=$( find $FOLDER -type f -name "$EXTENSION" -printf "%p\n"  2>/dev/null )
-
-found=0
 
 while read filename 
 do
@@ -38,8 +74,10 @@ do
         grep "$filename" sent_items.txt &>/dev/null \
         || for word in $( cat keywords.txt | tr '\n' ' ' )
         do
+            
             grep "$word" "$filename" &>/dev/null && {
-                found=1
+                echo "$filename" >> sent_items.txt
+                echo 1 > found.txt
                 keyword="$word"
                 subject=$( grep '^Subject' "$filename" | awk -F'Subject:' '{ print $2}' )
                 date=$( grep '^Date' "$filename" | awk -F'Date:' '{ print $2}' )
@@ -56,18 +94,25 @@ do
                     </td>
                 </tr>
                 " >> mail.html
-            }
+            }&
         done
-    } &
+    } 
 done <<< "$all_files"
  
 wait
 
 echo "
+</tbody>
 </table>
 </body>
 </html>
 " >> mail.html
 
-[[ $found -eq 1 ]] && /usr/sbin/sendmail -t soymatiasbarrios@gmail.com < mail.html
+sort sent_items.txt | uniq > tmp.txt && cat tmp.txt > sent_items.txt && rm -f tmp.txt
+
+[[ "$( cat found.txt )" == "1" ]] && { 
+    echo "Sending mail on $(date)...";
+    /usr/sbin/sendmail -t soymatiasbarrios@gmail.com < mail.html;
+    echo "Done";
+    }
 
